d53b7103df64feff0ccfd3a329f7d1af4755098e
Daniel B <76835848+Danielbp019@users.noreply.github.com>
2023-04-03 23:17:15 -0500

    Validaciones
    Comentarios de explicación en códigos y validaciones para diferentes campos como numero o texto

--- src/app/articulos/articulos.component.ts
+++ src/app/articulos/articulos.component.ts
@@ -12,13 +12,36 @@ import { FormControl, Validators } from '@angular/forms';
 
 export class ArticulosComponent implements OnInit {
 
+  /* 
+  public articulos = [] as any;
+  En el código que proporcionas, se define una variable articulos que se inicializa con un arreglo vacío [] y se tipa como any.
+
+  Esta variable se utiliza comúnmente en Angular para almacenar una lista de objetos de tipo IArticulo. La idea es que, cuando la aplicación cargue los datos de los artículos, estos se almacenen en el arreglo articulos.
+  */
   public articulos = [] as any;
   public selectedArticulo = <IArticulo>{};
   public modalTitle = '';
   public btnTitle = '';
+  //Aqui se agregan mas validaciones para los campos del formulario separadas por comas.
   public titulo = new FormControl('', Validators.required);
   public cuerpo = new FormControl('', Validators.required);
   public autor = new FormControl('', Validators.required);
+
+  /*  
+    Si recibes el error "Expected 0-3 arguments, but got 4" al agregar la validación de número mínimo, es posible que se deba a que la versión de Angular que estás utilizando no admite la función Validators.min() con cuatro argumentos.
+
+    Para solucionar esto, puedes usar la función Validators.min(0) que solo tiene un argumento, que es el valor mínimo permitido. De esta manera, se asegura que el valor mínimo permitido sea 0 y no se permitan valores negativos.
+
+      import { Validators } from '@angular/forms';
+      public SOLOnumeros = new FormControl(
+      '', [
+      Validators.required,
+      Validators.pattern('^[0-9]*$'), // Solo números permitidos
+      Validators.min(0) // Valor mínimo permitido es 0
+      ]);
+      public SOLOletras = public SOLOletras = new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z ]*$')] );
+  */
+
   public showError = false;
   modalRef?: BsModalRef;
 
@@ -48,6 +71,14 @@ export class ArticulosComponent implements OnInit {
     this.service.list()
       .subscribe(response => this.articulos = response);
   }
+
+  /* 
+    Este código parece ser una función llamada "delete" que borra un artículo utilizando Angular y RxJS.
+
+    La función toma un parámetro "articulo" del tipo "IArticulo" y llama al método "delete" del servicio "this.service" con el artículo como argumento. Luego, se suscribe a la respuesta del servicio utilizando el método "subscribe" y llama a la función "getList()" cuando recibe una respuesta exitosa.
+
+    Por lo tanto, la función "delete" probablemente borra un artículo utilizando el servicio y luego actualiza la lista de artículos mostrados en la vista. 
+  */
 
   delete(articulo: IArticulo) {
     this.service.delete(articulo)
--- src/app/services/articulos.service.ts
+++ src/app/services/articulos.service.ts
@@ -8,10 +8,18 @@ import { IArticulo } from '../articulos/articulos';
 })
 export class ArticulosService {
 
+  //URL del API donde vienen los datos.
   private _url = 'http://localhost:8000/api/articulos';
 
   constructor(private http: HttpClient) { }
 
+  /* 
+  Este código parece ser un método llamado "list" que devuelve un Observable de un arreglo de objetos del tipo "IArticulo" utilizando Angular y RxJS.
+
+  El método utiliza el servicio HttpClient de Angular para realizar una solicitud HTTP GET al endpoint especificado en la variable "_url". La respuesta de la solicitud es de tipo "Observable<IArticulo[]>" y se devuelve directamente desde el método.
+
+  En resumen, este método probablemente se utiliza para obtener una lista de objetos "IArticulo" desde un endpoint HTTP y devuelve un Observable para que el consumidor pueda suscribirse y recibir la respuesta de la solicitud.
+  */
   list(): Observable<IArticulo[]> {
     return this.http.get<IArticulo[]>(this._url);
   }
